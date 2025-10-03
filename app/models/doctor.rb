8b3c93dccc3733a44cd514bf34f0182a032aed48
Todiun <joquch@hotmail.co.uk>
2025-10-03 03:05:35 +0200

    Updated

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
 class Doctor < ApplicationRecord
+  belongs_to :city
   has_many :appointments
-  has_many :patients, through: :appointments
+  has_many :patients, through: :appointments$
+  
+  has_many :doctor_specialty
+  has_many :specialties, through: :doctor_specialties
 end
